ac01542dd6cc70427ccabb574155f639e22ef0f8
JcButNotJesus <jancornelis.pro@gmail.com>
2023-07-05 15:52:16 +0200

    add link basket and ignore confirmation

--- src/cores/components/atoms/basket-layer/BasketLayer.component.tsx
+++ src/cores/components/atoms/basket-layer/BasketLayer.component.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import type { Basket } from '../../../models/Basket';
 import './BasketLayer.component.scss';
 
@@ -6,8 +7,13 @@ interface BasketLayerComponentProps {
 }
 
 export const BasketLayerComponent = ({ currentBasket }: BasketLayerComponentProps) => (
+	<Link
+			to='/order-confirmation'
+			data-testid='to-confirmation'
+		>
 	<div className='basket-layer'>
 		<div className='basket-layer__restaurant-name'>{currentBasket?.restaurantName}</div>
 		<div className='basket-layer__description'>Panier en cours ...</div>
 	</div>
+	</Link>
 );
--- src/cores/components/atoms/basket-layer/BasketLayer.container.tsx
+++ src/cores/components/atoms/basket-layer/BasketLayer.container.tsx
@@ -1,8 +1,10 @@
+import { useLocation } from 'react-router-dom';
 import { useBasketContext } from '../../../contexts/basket/Basket.context';
 import { BasketLayerComponent } from './BasketLayer.component';
 
 export const BasketLayerContainer = () => {
+	const location = useLocation();
 	const { currentBasket } = useBasketContext();
 
-	return currentBasket ? <BasketLayerComponent currentBasket={currentBasket} /> : <div />;
+	return currentBasket && location.pathname!="/order-confirmation" ? <BasketLayerComponent currentBasket={currentBasket} /> : <div />;
 };
